937cce3cd950425074e7acdbb5fa10b28d35744f
Nathan Cassee <nathancassee@gmail.com>
2018-10-11 13:18:09 +0200

    Added the code that removes failed projects so that they can be scraped again

--- dataset_maintenance.py
+++ dataset_maintenance.py
@@ -52,9 +52,20 @@ pull_requests_collection = database["pull_requests"]
 
 projects_collection = database["projects"]
 
+commits_collection = database["commits"]
+
 for failed_project in projects_collection.find({'succeeded': False}):
     print("Deleting {}".format(failed_project["full_name"]))
     split_name = failed_project["full_name"].split("/")
+    
+    for pr in pull_requests_collection.find({'project_name':
+        split_name[1], 'project_owner':
+            split_name[0]}):
+        for sha in pr["commits"]:
+            commits_collection.delete_one({'sha':sha})
+    
     pull_requests_collection.delete_many({'project_name':
         split_name[1], 'project_owner':
             split_name[0]})
+    
+    projects_collection.delete_one({'full_name':failed_project["full_name"]})
