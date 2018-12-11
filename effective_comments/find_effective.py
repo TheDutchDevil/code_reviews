7d3ad64f34a84f17f08fe7d778c23408818af01c
Nathan Cassee <nathancassee@gmail.com>
2018-12-11 10:30:14 +0100

    Added the 0.1 of code that attempts to find effective comments

---
+++ effective_comments/find_effective.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 11 10:22:54 2018
+
+@author: natha
+"""
+
+import re
+from pymongo import MongoClient
+
+mongo_client = MongoClient()
+
+database = mongo_client["graduation"]
+
+commits_collection = database["commits"]
+
+line_number_hunk_regex_raw = "((^|\\n)\@\@\ -(\d+(|,\d+))\ \+(\d+(|,\d+))\ \@\@)"
+
+line_number_hunk_regex = re.compile(line_number_hunk_regex_raw)
+
+'''
+Given an item first checks whether it is a comment or 
+commit. Then return the correct date field for the item. 
+'''
+def return_date_for_pr_or_commit(item):
+    if "sha" in item:
+        return item["date"]
+    elif "original_commit_id" in item:
+        return item["created_at"]
+    else:
+        raise ValueError("Ehm, this is not a code comment or commit")
+        
+        
+'''
+We do a sort of sweep line algorithm, where we sort commits and line 
+comments on the time they have been posted. We go through them both 
+chronologically, for a code comment we note its position, for a commit
+we extract the position of the diff(s) and we note whether they 
+intersect with any of the existing comments. If they intersect, we 
+mark the comment as having been change inducing. 
+
+Furthermore, we update the position of existing comments to account
+for the lines that have been added or removed in diffs. 
+'''
+def process_pr(pr):
+    
+    comments = sorted([rc for rc in pr["review_comments"] if rc["in_reply_to_id"] is None], key=lambda comment : comment["created_at"])
+    commits = list([commits_collection.find_one({'sha': commit, 'date': {'$exists': True}}) for commit in pr["commits"]])
+    
+    commits = list([cmt for cmt in commits if cmt is not None])
+       
+    commits.sort(key = lambda commit : commit["date"])
+    
+    if len(comments) == 0:
+        return
+        
+    if len(commits)  == 0:
+        print("\tOh misery! PR with number {} has no dated commits".format(pr["number"]))
+        return
+        
+    merged_list = commits + comments
+    
+    merged_list.sort(key = return_date_for_pr_or_commit)
+    
+    placed_line_comments = []
+    
+    made_commits = {}
+    
+    for item in merged_list:
+        # Only process newly placed comments, don't bother with comments
+        # that are placed as a reaction
+        if "original_commit_id" in item:
+            # Add the comment on the moment it has been placed and
+            # attribute it with the closest location in the new part of the 
+            # diff
+            placed_line_comments.append(item)
+
+            lines = item["diff_hunk"].split("\n")
+            
+            curr_pos_in_old = 0
+            curr_pos_in_new = 0
+            
+            lines_done = 0
+            
+            for line in lines:                
+                # The below bit updates the lines based on the counter
+                if line.startswith("@@"):
+                    hunk_header_match = line_number_hunk_regex.match(line)
+                    
+                    curr_pos_in_old = int(hunk_header_match.group(3).split(",")[0])
+                                        
+                    curr_pos_in_new = int(hunk_header_match.group(5).split(",")[0])
+                    
+                elif line.startswith("-"):
+                    curr_pos_in_old += 1
+                elif line.startswith("+"):
+                    curr_pos_in_new += 1
+                elif line.startswith(" "):
+                    curr_pos_in_old += 1
+                    curr_pos_in_new += 1
+                elif line.startswith("\\"):
+                    pass
+                else:
+                    raise ValueError("Horror, there is diff panic")
+                    
+                lines_done += 1
+                
+                if item["original_position"] == lines_done:
+                    # This is where the comment is --> record
+                    # the position in the new file as the location 
+                    # of this comment. 
+                    #
+                    # That way we can compare later commits to see
+                    # if they came close to this comment
+                    
+                    item["eff_track_line"] = curr_pos_in_new
+        
+            
+        elif "sha" in item:
+            
+            made_commits[item["sha"]] = item
+            
+            for file in item["files"]:
+                # If the file has been modified we need to do two things.
+                # Check if any omments were near any of the lines that have
+                # been changed in this commit.
+                # and if lines have been added or removed we need to update
+                # the location of currently placed comments to reflect the
+                # fact that they have changed by position by this commit
+                if file["status"] == 'modified':
+                    
+                    for file_comment in [rc for rc in placed_line_comments if rc["path"] == file["filename"] and "eff_track_line" in rc]:
+                        patch = file["patch"]
+                        
+                        if patch is None:
+                            # This is a serious error condition!
+                            return
+                        
+                        curr_pos_in_old = 0
+                        curr_pos_in_new = 0
+                        
+                        lines_delta = 0
+                        
+                        for line in patch.split("\n"):
+                            if line.startswith("@@"):
+                                hunk_header_match = line_number_hunk_regex.match(line)
+
+                                curr_pos_in_old = int(hunk_header_match.group(3).split(",")[0])
+
+                                curr_pos_in_new = int(hunk_header_match.group(5).split(",")[0])
+                                
+                                # Positive for added lines
+                                # negative for removed lines
+                                lines_delta = curr_pos_in_new - curr_pos_in_old
+                                
+                                # At this point the diff passed the location of the comment. 
+                                # We update the old position and we're done.
+                                if curr_pos_in_old > file_comment["eff_track_line"]:
+                                    file_comment["eff_track_line"] += lines_delta
+
+                            elif line.startswith("-"):
+                                curr_pos_in_old += 1
+                            elif line.startswith("+"):
+                                curr_pos_in_new += 1
+                            elif line.startswith(" "):
+                                curr_pos_in_old += 1
+                                curr_pos_in_new += 1
+                            elif line.startswith("\\"):
+                                pass                                                 
+                            else:
+                                print(line)
+                                raise ValueError("Horror, there is diff panic")
+
+                            if file_comment["eff_track_line"] == curr_pos_in_old:
+                                #print("EFFECTIVE {}".format(file_comment["url"]))
+                                
+                            
+                # If the file is deleted any comments in the 
+                # file have induced some form of change
+                elif file["status"] == "removed":
+                    for file_comment in [rc for rc in placed_line_comments if rc["path"] == file["filename"]]:
+                        """print("body: {}".format(file_comment["body"]))
+                        print("sha: {}".format(item["sha"]))
+                        print("filename: {}".format(file["filename"]))
+                        print("url: {}".format(pr["html_url"]))
+                        print("found effective comment")"""
+                        #print("EFFECTIVE {}".format(file_comment["url"]))
+                        placed_line_comments.remove(file_comment)
+                    
+        else:
+            raise ValueError("Ehm this is not a code comment or commit")
+    
+
