e61e58e286f71ce58452297bbbe8a1d26a952300
Nathan Cassee <nathancassee@gmail.com>
2018-12-14 10:04:45 +0100

    Build logs can be scraped using travis

--- analysis/travis/scrape_build_logs.py
+++ analysis/travis/scrape_build_logs.py
@@ -25,7 +25,7 @@ TRAVIS_HEADER = {'Travis-API-Version' : '3',
 
 
 def retrieve_build_for_identifier(build_identifier):
-    builds_url = "{}/{}/{}".format(BASE_TRAVIS_URL, 'builds', build_identifier)
+    builds_url = "{}/{}/{}".format(BASE_TRAVIS_URL, 'build', build_identifier)
     
     build_response = requests.get(builds_url, headers = TRAVIS_HEADER)
     
@@ -42,8 +42,37 @@ def retrieve_build_for_identifier(build_identifier):
     build_data = build_response.json()
     
     if build_data['@type'] == 'error':
-        print(build_data)
-        raise ValueError("Could not scrape build from Travis")
+        raise ValueError("Could not scrape build from Travis response was {}"
+                         .format(build_data))
         
+    return build_data
+
+def extract_job_ids_for_build(build_data):
+    if len(build_data["jobs"]) == 0:
+        raise ValueError("No jobs for this build")
     
+    job_ids = []
+    
+    for job in build_data["jobs"]:
+        job_ids.append(job["id"])
+        
+    return job_ids
+        
+
+def retrieve_build_log_for_job_id(job_identifier):
+    build_log_url = "{}/{}/{}/log".format(BASE_TRAVIS_URL, "job", job_identifier)
+    
+    actual_headers = dict(TRAVIS_HEADER)
+    
+    # This is needed so that the build log is not in some weird decoded unreadable
+    # format
+    actual_headers["Accept"] = "text/plain"
+    
+    log_response = requests.get(build_log_url, headers = actual_headers)
+    
+    if log_response.status_code not in [200]:
+        print("Could not retrieve build log from Travis, status code is {}".
+              format(log_response.status_code))
+        
+    return log_response.text
                 
