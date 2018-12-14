a47bb1fc224b2c6e9aa76f11c989e2fa99e27edf
Nathan <nathancassee@gmail.com>
2018-12-14 04:17:39 -0500

    Script that updates two custom columns in ghtorrent with number of comments and pull requests

---
+++ run_counts.py
@@ -0,0 +1,32 @@
+import MySQLdb
+db = MySQLdb.connect(passwd="ghtorrent_restore",db="ghtorrent_restore",user="ghtorrent")
+
+c = db.cursor()
+
+c.execute("Select max(projects.id), min(projects.id) from projects where total_number_pr_comments = 0")
+
+counts = c.fetchone()
+
+min_proj = counts[1]
+max_proj = counts[0]
+
+for i in range(min_proj, max_proj + 1):
+    c.execute("""update projects
+                    set projects.total_number_pr_comments = ( select count(projects.id)
+                        from 
+                        pull_requests pr, 
+                        issues iss, 
+                        issue_comments iss_cms
+                        where 
+                            projects.id = pr.base_repo_id and
+                            pr.id = iss.pull_request_id and
+                            iss.pull_request = true and
+                            iss_cms.issue_id = iss.id         
+                            group by projects.id ), 
+                    projects.total_number_prs = ( select count(projects.id)
+                            from 
+                            pull_requests pr
+                            where projects.id = pr.base_repo_id      
+                            group by projects.id )
+                        where projects.id = {} and total_number_pr_comments = 0
+        """.format(i))
