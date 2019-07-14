c04054219af0b65f7a5fd62651fd0de616867f50
Nathan Cassee <nathancassee@gmail.com>
2019-07-14 13:53:44 +0200

    reworked the gh_token infrastructure

--- analysis/icse/tokens/gh_tokens.py
+++ analysis/icse/tokens/gh_tokens.py
@@ -6,5 +6,31 @@ Created on Wed Sep 12 17:04:10 2018
 """
 
 import queue
+import threading
+import time
 
 gh_tokens = queue.Queue()
+
+__running_timer__ = False
+
+tokens_added = []
+
+def read_tokens():
+    with open('gh_tokens_list') as fp:  
+        for cnt, line in enumerate(fp):
+            if line == "" or "," not in line:
+                continue
+
+            token = line.split(",")[0]
+            comment = line.split(",")[1]
+
+            if token not in tokens_added:
+                tokens_added.append(token)
+                gh_tokens.put(token)
+                print("Added {}'s token".format(comment))
+
+read_tokens()
+
+if not __running_timer__:    
+    threading.Timer(10.0, read_tokens).start()
+
